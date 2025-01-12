2b898faae649ed36255113b817a3a15c07f2adbd
Alexander Malikov <malikoff.at@gmail.com>
2025-01-13 00:30:45 +0300

    Added configuration of import settings

---
+++ ansible/scripts/configure_import.rb
@@ -0,0 +1,8 @@
+# Enable or disable the GitHub import feature
+ApplicationSetting.current.update!(github_import_enabled: true)
+# Enable or disable the GitLab import feature
+ApplicationSetting.current.update!(gitlab_import_enabled: true)
+# Enable or disable the Gogs import feature
+ApplicationSetting.current.update!(gogs_import_enabled: true)
+# Enable or disable the url import feature
+ApplicationSetting.current.update!(url_import_enabled: true)
